1a01551d0abf90b23aa0923f630ae9d50afdfebf
pkulijing <pkuyplijing@gmail.com>
2016-02-28 23:42:33 +0100

    Adjusted to new algs4 package

--- UnionFind/src/WeightedQuickUnion.java
+++ UnionFind/src/WeightedQuickUnion.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 public class WeightedQuickUnion {
 	
 	private int[] id;
@@ -6,30 +8,26 @@ public class WeightedQuickUnion {
 	
 	public int count() { return count;}
 	
-	public WeightedQuickUnion(int n)
-	{
+	public WeightedQuickUnion(int n) {
 		count = n;
 		id = new int[n];
 		size = new int[n];
-		for(int i = 0; i < n; i++)
-		{
+		for(int i = 0; i < n; i++) {
 			id[i] = i;
 			size[i] = 1;
 		}
 	}
 	
-	public boolean connected(int p, int q) { return find(p) == find(q);}
+	public boolean connected(int p, int q) { return find(p) == find(q); }
 
-	public int find(int p) 
-	{
+	public int find(int p)  {
 		while(id[p] != p)
 			p = id[p];
 		return p;
 	}
 	
 	
-	public void union(int p, int q)
-	{	
+	public void union(int p, int q) {	
 		int rootp = find(p);
 		int rootq = find(q);
 		
